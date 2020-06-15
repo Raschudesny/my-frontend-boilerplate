3aa59012e9c23e19d541fcef10fc1ba7386265a2
Raschudesny <rifkinsm@gmail.com>
2020-06-15 11:42:21 +0300

    Update index.tsx (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-export default function init() {
-  ReactDOM.render(<App/>, document.getElementById("rootId"));
-}
+ReactDOM.render(<App/>, document.getElementById("rootId"));
+
